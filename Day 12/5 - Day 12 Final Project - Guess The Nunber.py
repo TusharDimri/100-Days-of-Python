ddc223d5c46fef0a2d253e76cec40f103a62f4d8
Tushar <tushar.dimri22@gmail.com>
2021-09-25 12:35:27 +0530

    Commit

--- Day 12/5 - Day 12 Final Project - Guess The Nunber.py
+++ Day 12/5 - Day 12 Final Project - Guess The Nunber.py
@@ -1,28 +1,35 @@
 from art import logo
 import random
 from os import system
-
-# print(logo)
+from time import sleep
 
 def guess_the_number(attempts):
     while attempts > 0:
-        print(f"You have {attempts} attempts remaining.")    
+        if attempts == 1:
+            print(f"You have {attempts} attempt remaining.")    
+        else:
+            print(f"You have {attempts} attempts remaining.")    
+            
         guess = int(input("Make a guess: "))
 
         if guess < guess_num:
-            print("Too Low.\nGuess Again.")
+            print("Too Low.")
+            if attempts > 1:
+                print("Guess Again.")
             attempts -= 1
 
         elif guess > guess_num:
-            print("Too High.\nGuess Again.")
+            print("Too High.")
+            if attempts > 1:
+                print("Guess Again.")
             attempts -= 1
 
         elif guess == guess_num:
-            print(f"You got it! The anser was {guess_num}.")
+            print(f"You got it! The answer was {guess_num}.")
             break
 
     if attempts == 0:
-        print("You've run out of guesses, you lose.")
+        print(f"You've run out of guesses, you lose. The correct answer was {guess_num}")
 
 
 wantingToPlay = True
@@ -37,6 +44,11 @@ while wantingToPlay:
         attempts = 10
     elif choice == 'hard':
         attempts = 5
+    else:
+        print("Invalid Input.")
+        sleep(2)
+        _ = system('cls')
+        continue
 
     guess_the_number(attempts)
 
