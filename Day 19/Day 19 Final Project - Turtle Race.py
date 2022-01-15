2c5bc4ed8012e363a3fb19b80ac09d06f10a5650
Tushar <tushar.dimri22@gmail.com>
2022-01-15 22:29:54 +0530

    Commit

---
+++ Day 19/Day 19 Final Project - Turtle Race.py
@@ -0,0 +1,47 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(height=500, width=500)
+
+colors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"]
+
+class Tutles(object):
+    pass
+
+user_bet = screen.textinput(title="Make a bet: ", prompt="Which Turtle will win the race? Enter a color:")
+print(user_bet)
+
+turtles = []
+y_pos = [120, 80, 40, 0, -40, -80, -120]
+
+for i in range(7):
+    t = Turtle(shape="turtle")
+    t.speed("slow")
+    t.penup()
+    t.goto(x = -230, y = y_pos[i])
+    t.speed("slowest")
+    # t.shape("turtle")
+    turtle_color = random.choice(colors)
+    colors.remove(turtle_color)
+    t.color(turtle_color)
+    turtles.append(t)
+
+if user_bet:
+    isRaceOn = True
+
+while isRaceOn:
+    for ts in turtles:
+        random_distance = random.randint(1, 10)
+        ts.forward(random_distance)
+
+        if ts.xcor() > 230:
+            winner = ts.pencolor()
+            if winner == user_bet:
+                print("You Won!")
+            else:
+                print(f"{winner.capitalize()} turtle won. You Lose.")
+            isRaceOn = False
+
+
+screen.exitonclick()
