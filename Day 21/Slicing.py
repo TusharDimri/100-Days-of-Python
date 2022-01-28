dd2c45e750d49296e28cc2b4c4161a89c99e9d44
Tushar <tushar.dimri22@gmail.com>
2022-01-28 22:12:46 +0530

    Commit

--- Day 21/Slicing.py
+++ Day 21/Slicing.py
@@ -3,5 +3,6 @@ musical_notes = ["A", "B", "C", "D", "E", "F", "G"]
 print(musical_notes[1:5])
 print(musical_notes[1:])
 print(musical_notes[:6])
+print(musical_notes[:6: 2])
 print(musical_notes[: : -1])
 print(musical_notes[3: 4: ])
