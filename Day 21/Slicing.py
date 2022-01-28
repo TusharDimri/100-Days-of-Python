faa7283320b690bfe1b804baff2040b57d3e6bae
Tushar <tushar.dimri22@gmail.com>
2022-01-28 22:19:10 +0530

    Commit

--- Day 21/Slicing.py
+++ Day 21/Slicing.py
@@ -1,4 +1,5 @@
 musical_notes = ["A", "B", "C", "D", "E", "F", "G"]
+major_scale = ("Do", "Re", "Mi", "Fa", "So", "La", "Ti") 
 
 print(musical_notes[1:5])
 print(musical_notes[1:])
@@ -6,3 +7,8 @@ print(musical_notes[:6])
 print(musical_notes[:6: 2])
 print(musical_notes[: : -1])
 print(musical_notes[3: 4: ])
+
+print(major_scale[::2])
+print(major_scale[1:5:2])
+print(major_scale[::3])
+print(major_scale[0::-1])
