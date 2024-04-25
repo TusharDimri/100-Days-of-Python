a1d4da7df13a62e754fb915849c81a3635925888
TusharDimri <tushar.dimri22@gmail.com>
2024-04-25 22:45:55 +0530

    Complete Day 50 and Day 51

---
+++ Day 51/Day 51 Final Project - Speed Test Complaint Twitter.py
@@ -0,0 +1,59 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+PROMISED_UP = 150
+PROMISED_DOWN = 150
+
+class InternetSpeedTwitterBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.up = ""
+        self.down = ""
+
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net")
+        time.sleep(20)
+        accept_button = self.driver.find_element(By.ID, value="onetrust-accept-btn-handler")
+        accept_button.click()
+        start = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a')
+        start.click()
+        time.sleep(60)
+        self.up = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
+        self.down = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        print(self.up)
+        print(self.down)
+
+    def tweet(self):
+        self.driver.get("https://twitter.com/home?lang=en")   
+        time.sleep(10)      
+        sign_in_btn = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div/div/div[3]/div[5]/a/div')
+        sign_in_btn.click()
+        time.sleep(10)
+        username = self.driver.find_element(By.XPATH, value='//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')
+        username.send_keys("")
+        next_btn = self.driver.find_element(By.XPATH, value='//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]')
+        next_btn.click()
+        time.sleep(10)
+        password = self.driver.find_element(By.XPATH, value='//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[2]/div/label/div/div[2]/div[1]/input')
+        password.send_keys("")
+        log_in_btn = self.driver.find_element(By.XPATH, value='//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div')
+        log_in_btn.click()
+        
+        time.sleep(10)
+        tweet_input = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_input.send_keys(tweet)
+        post_btn = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/div[3]')
+        post_btn.click()
+
+        time.sleep(500)
+
+bot = InternetSpeedTwitterBot()
+bot.get_internet_speed()
+bot.tweet()
+
